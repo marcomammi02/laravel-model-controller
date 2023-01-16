7c25369fb999508d5b9c7fb734a105d8240e8de2
marcomammi02 <marcomammi2002@gmail.com>
2023-01-16 19:51:47 +0100

    Finish

--- resources/views/guest/page.blade.php
+++ resources/views/guest/page.blade.php
@@ -5,18 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Page</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <ul>
-        @foreach ($movies as $movie)
-        <li>
-            <h1>{{ $movie->title }}</h1>
-            <p>Original title: {{ $movie->original_title }}</p>
-            <p>Nationality: {{ ucfirst($movie->nationality) }}</p>
-            <p>Date: {{ $movie->date }}</p>
-            <p>Vote: {{ $movie->vote }}</p>
-        </li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <ul>
+            @foreach ($movies as $movie)
+            <li>
+                <div class="card">
+                    <h1>{{ $movie->title }}</h1>
+                    <p>Original title: {{ $movie->original_title }}</p>
+                    <p>Nationality: {{ ucfirst($movie->nationality) }}</p>
+                    <p>Date: {{ $movie->date }}</p>
+                    <p>Vote: {{ $movie->vote }}</p>
+                </div>
+            </li>
+            @endforeach
+        </ul>
+    </div>
 </body>
 </html>
